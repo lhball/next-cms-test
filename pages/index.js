c14b534e09a102b5a209cff61b0a69c98f6d4c2e
Luis <luis.ball@envizzo.com>
2020-01-30 19:55:04 -0500

    add brand images and editoral flow

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default class Home extends Component {
               <li key={k}>
                 <h2>{brand.name}</h2>
                 <p>{brand.description}</p>
+                <p>{brand.image}</p>
               </li>
             ))}
           </ul>
